4648502bd5d2945ecf31647caf28bcf035a91a21
= <=>
2024-03-27 11:29:28 -0300

    act1y2

--- 02.Condicionales/02-condicionales.js
+++ 02.Condicionales/02-condicionales.js
@@ -2,15 +2,23 @@ function verificarNumero(numero){
     // Escribe un programa que tome un número como entrada y muestre "Es positivo" si el número es mayor que cero,
     // "Es negativo" si es menor que cero y "Es cero" si es igual a cero.
     // Tu código 👇:
-
+    if (numero>0) {
+        return "Es positivo";
+    } else if (numero<0) {
+        return "Es negativo";
+    } else {
+        return "Es cero";
 }
-
+}
 
 function esBisiesto(anio){
     // Escribe un programa que determine si un año es bisiesto o no. (true o false)
     // Un año es bisiesto si es divisible entre 4, excepto aquellos que son divisibles entre 100 pero no entre 400.
     // Tu código 👇:
-
+    if (anio%4==0 && (anio%100!=0 || anio%400==0)) {
+        return true;
+    } else {
+        return false;
 }
 
 function verificarCalificacion(calificacion){
@@ -18,22 +26,37 @@ function verificarCalificacion(calificacion){
     // El estudiante aprueba si su calificación es mayor o igual a 60.
     // Devolver "Aprobó" o "Reprobó"
     // Tu código 👇:
-
-}
+    if (calificacion >=60) {
+        return "Aprobó";
+    } else {
+        return "Reprobó";
+    }
+    }
 
 function esMayor(num1, num2, num3){
     // Escribe un programa que determine el mayor de tres números dados.
     // Devolver el numero mayor
     // Tu código 👇:
+    if (num1>num2 && num1>num3) {
+        return num1;
+    }  else if (num2>num1 && num2>num3) {
+            return num2
+        } else {
+            return num3;
+        }
+    }
 
-}
 
 function esPrimo(num){
     // Escribe un programa que determine si un número dado es primo.
     // Devolver true o false
     // Tu código 👇:
-
-}
+    if (num%2!=0 && num%3!=0 && num%5!=0 && num%7!=0) {
+        return true;
+    } else {
+        return false;
+    }
+    }
 
 module.exports = {
     verificarCalificacion,
